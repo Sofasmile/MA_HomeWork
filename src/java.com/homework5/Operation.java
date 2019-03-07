347d69cdb3cb3abaa8e144f48dcab33106898141
Sofia Diakonova <36365171+Sofasmile@users.noreply.github.com>
2019-03-07 15:32:55 +0200

    created calculator without switch-case and if-else

---
+++ src/java.com/homework5/Operation.java
@@ -0,0 +1,29 @@
+package homework5;
+
+import java.util.function.DoubleBinaryOperator;
+
+public enum Operation {
+    ADD("+", (d1, d2) -> d1 + d2),
+    SUB("-", (d1, d2) -> d1 - d2),
+    MUL("*", (d1, d2) -> d1 * d2),
+    DIV("/", (d1, d2) -> d1 / d2),
+    RAIS("^", (d1, d2) -> Math.pow(d1, d2)),
+    SQR("√", (d1, d2) -> Math.pow(d1, 1 / d2)), //Alt+251 = "√"
+    SQR_MOD("#", (d1, d2) -> Math.pow((d1 + d2) / d1 + 117, d2));
+
+    private final String symbol;
+    private final DoubleBinaryOperator operator;
+
+    Operation(String symbol, DoubleBinaryOperator operator) {
+        this.symbol = symbol;
+        this.operator = operator;
+    }
+
+    public double getResult(double d1, double d2) {
+        return operator.applyAsDouble(d1, d2);
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+}

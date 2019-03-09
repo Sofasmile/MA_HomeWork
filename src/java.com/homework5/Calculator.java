7a45e14218086ddcdfb46d0dc770185764f9679f
Sofia Diakonova <36365171+Sofasmile@users.noreply.github.com>
2019-03-09 16:03:39 +0200

    created calculator without switch-case and if-else

--- src/java.com/homework5/Calculator.java
+++ src/java.com/homework5/Calculator.java
@@ -1,27 +1,5 @@
 package homework5;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class Calculator {
-    public void calculate(double a, double b, char c) {
-        List<Double> values = new ArrayList<Double>();
-        values.add(a);
-        values.add(b);
-        String operator = String.valueOf(c);
-        try {
-            System.out.println(mathOperation(values, operator));
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
-    }
 
-    private Double mathOperation(List<Double> values, String operator) {
-        Operation result = Arrays.stream(Operation.values())
-                .filter(op -> op.getSymbol().equals(operator))
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("This operator doesn't exist"));
-        return result.getResult(values.get(0), values.get(1));
-    }
 }
--- src/java.com/homework5/Program.java
+++ src/java.com/homework5/Program.java
@@ -2,12 +2,5 @@ package homework5;
 
 public class Program {
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
-        calculator.calculate(5.9, 2.1, '+');
-        calculator.calculate(1.3, 2.2, '-');
-        calculator.calculate(7.3, 2.5, '*');
-        calculator.calculate(20.0, 6.5, '/');
-        calculator.calculate(25.0, 2.0, '√');
-        calculator.calculate(15.0, 3.0, '#');
     }
 }

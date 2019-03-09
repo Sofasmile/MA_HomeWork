95900401879c163bfa7cb8dd6dbbb7c1ab538126
Sofia Diakonova <36365171+Sofasmile@users.noreply.github.com>
2019-03-09 20:58:54 +0200

    maked method static and returned double type

--- src/java.com/homework5/Calculator.java
+++ src/java.com/homework5/Calculator.java
@@ -3,11 +3,11 @@ package homework5;
 import java.util.stream.Stream;
 
 public class Calculator {
-    public void calculate(double a, double b, char c) {
+    public static double calculate(double a, double b, char c) {
         Operation result = Stream.of(Operation.values())
                 .filter(op -> op.getSymbol() == c)
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("This operator doesn't exist"));
-        System.out.println(result.getResult(a, b));
+        return result.getResult(a, b);
     }
 }
--- src/java.com/homework5/Program.java
+++ src/java.com/homework5/Program.java
@@ -2,12 +2,11 @@ package homework5;
 
 public class Program {
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
-        calculator.calculate(5.9, 2.1, '+');
-        calculator.calculate(1.3, 2.2, '-');
-        calculator.calculate(7.3, 2.5, '*');
-        calculator.calculate(20.0, 6.5, '/');
-        calculator.calculate(25.0, 2.0, '√');
-        calculator.calculate(15.0, 3.0, '#');
+        System.out.println(Calculator.calculate(5.9, 2.1, '+'));
+        System.out.println(Calculator.calculate(1.3, 2.2, '-'));
+        System.out.println(Calculator.calculate(7.3, 2.5, '*'));
+        System.out.println(Calculator.calculate(20.0, 6.5, '/'));
+        System.out.println(Calculator.calculate(25.0, 2.0, '√'));
+        System.out.println(Calculator.calculate(15.0, 3.0, '#'));
     }
 }

312e6954509faed57b30db7739343ac705525909
Sofia Diakonova <36365171+Sofasmile@users.noreply.github.com>
2019-03-17 12:35:08 +0200

    corrected the class Singleton

--- src/java.com/multithreading/queues/BlockingQueueDemo.java
+++ src/java.com/multithreading/queues/BlockingQueueDemo.java
@@ -6,6 +6,7 @@ import java.util.concurrent.BlockingQueue;
 public class BlockingQueueDemo {
     public static final int COUNT = 5;
     public static final int CAPACITY = 10;
+
     public static void main(String[] args) {
         BlockingQueue bq = new ArrayBlockingQueue(CAPACITY);
         new Thread(new Producer(bq, COUNT)).start();
--- src/java.com/multithreading/singleton/Singleton.java
+++ src/java.com/multithreading/singleton/Singleton.java
@@ -3,6 +3,9 @@ package multithreading.singleton;
 public class Singleton {
     private static volatile Singleton instance;
 
+    private Singleton() {
+    }
+
     public static Singleton getInstance() {
         Singleton localInstance = instance;
         if (localInstance == null) {
